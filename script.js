a85fc4fcaaec6d5acb68f742ea7059d5912c0d9d
avilavida <avilavida@uoc.edu>
2025-03-27 10:37:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ d3.selectAll("h1").remove();
 
 var margin = {top: 0, right: 150, bottom: 100, left: 150},
   width = window.innerWidth - margin.left - margin.right,
-  height = 750 - margin.top - margin.bottom;
+  height = 650 - margin.top - margin.bottom;
 
   d3.select("#my_dataviz")
   .style("display", "flex")
